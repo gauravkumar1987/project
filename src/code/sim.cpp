c68f01783762ef711fb8b9e77d2d2eb611787e29
Gaurav Kumar <gk.chadha@ymail.com>
2015-06-18 11:48:36 +0530

    init commit

---
+++ src/code/sim.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+//#include<sim.h>
+using namespace std;
+
+
+bool disp()
+{
+cout<<"show called";
+return true;
+}
+
+bool show()
+{
+cout<<"show called";
+return false;
+}
+
+
+
+
+
+
+
---
+++ src/code/sim.h
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+bool disp();
+bool show();
+
+
+/*class sims
+{
+public:
+bool disp();
+bool show();
+};*/
