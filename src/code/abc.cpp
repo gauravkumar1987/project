3d0b4c83719473f1f7b51d27af6638e306bd3423
Gaurav Kumar <gk.chadha@ymail.com>
2015-06-18 11:56:17 +0530

    Project : C++ Purpose : Initial commit

---
+++ src/code/abc.cpp
@@ -0,0 +1,18 @@
+#include<gmock/gmock.h>
+
+#include<iostream>
+#include<sim.h>
+using namespace std;
+
+
+class abc
+{
+
+public:
+	int m_value;
+	abc(int a)
+		{
+		m_value=a;		
+		}
+
+};
---
+++ src/code/main.cpp
@@ -0,0 +1,10 @@
+#include<sim.h>
+
+#include<iostream>
+using namespace std;
+int main()
+{
+disp();
+show();
+
+}
